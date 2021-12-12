efefd74954ddeb65ca1041264531a8eaa1c96838
Muhammad Ahmed Ali Asim <70942865+ahmedaliasim@users.noreply.github.com>
2021-12-12 15:07:18 +0500

    edit bcrypt y

--- app.js
+++ app.js
@@ -53,3 +53,9 @@ app.get('/',requireToken,(req,res)=>{
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
